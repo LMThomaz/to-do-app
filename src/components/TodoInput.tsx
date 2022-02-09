f037f91f942ed5a4b65a3b90cd3194336f2cb884
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2022-02-08 21:31:47 -0300

    completed challange

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -7,32 +7,36 @@ interface TodoInputProps {
 }
 
 export function TodoInput({ addTask }: TodoInputProps) {
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
 
   function handleAddNewTask() {
-    //TODO - Call addTask if task not empty and clean input value 
+    if (task.trim() === '') return;
+
+    addTask(task);
+    setTask('');
   }
 
   return (
     <View style={styles.inputContainer}>
-      <TextInput 
-        style={styles.input} 
-        placeholder="Adicionar novo todo..."
-        placeholderTextColor="#B2B2B2"
-        returnKeyType="send"
-        selectionColor="#666666"
-        //TODO - use value, onChangeText and onSubmitEditing props
+      <TextInput
+        style={styles.input}
+        placeholder='Adicionar novo todo...'
+        placeholderTextColor='#B2B2B2'
+        returnKeyType='send'
+        selectionColor='#666666'
+        value={task}
+        onChangeText={setTask}
+        onSubmitEditing={handleAddNewTask}
       />
       <TouchableOpacity
-        testID="add-new-task-button"
+        testID='add-new-task-button'
         activeOpacity={0.7}
         style={styles.addButton}
-        //TODO - onPress prop
-      >
-        <Icon name="chevron-right" size={24} color="#B2B2B2" />
+        onPress={handleAddNewTask}>
+        <Icon name='chevron-right' size={24} color='#B2B2B2' />
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -53,7 +57,7 @@ const styles = StyleSheet.create({
     borderBottomLeftRadius: 5,
     borderRightWidth: 1,
     borderRightColor: '#EBEBEB',
-    color: '#666666'
+    color: '#666666',
   },
   addButton: {
     backgroundColor: '#FFF',
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,29 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map((task) => {
+      if (task.id !== id) return task;
+
+      return { ...task, done: !task.done };
+    });
+
+    setTasks([...updatedTasks]);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+
+    setTasks([...updatedTasks]);
   }
 
   return (
@@ -26,18 +40,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
